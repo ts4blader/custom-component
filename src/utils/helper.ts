a43e2bbdc6a69bdf5030066b9e9a62519b545260
ts4blader <ts4blader@gmail.com>
2023-07-09 15:34:01 +0700

    :tada: initial

---
+++ src/utils/helper.ts
@@ -0,0 +1,6 @@
+import { cx } from "class-variance-authority"
+import { ClassNameValue, twMerge } from "tailwind-merge"
+
+export function cn(...inputs: ClassNameValue[]) {
+  return twMerge(cx(inputs))
+}
